f71228477b915253ba79e3732c58007416f7e409
Robert Lukotka <relatko@gmail.com>
2021-03-04 20:19:40 +0100

    Adding edges

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-from pyrsistent import pmap, pset
+from pyrsistent import pmap, pset, thaw
 
 #you may want to use PClass, but we want to learn something
 class Graph:
@@ -25,3 +25,12 @@ class Graph:
         else:
             return self._newgraph(self._incidence.set(v, pset()))
             
+    def hasedge(self, v1, v2):
+        return v2 in self._incidence[v1]
+            
+    def addedge(self, v1, v2):
+        evo = self._incidence.evolver()
+        evo[v1] = evo[v1].add(v2)
+        evo[v2] = evo[v2].add(v1)
+        inc = evo.persistent()
+        return self._newgraph(inc)
