1718b7de886fcfee5e53da8850f1b0cef83c6b05
Robert Lukotka <relatko@gmail.com>
2021-03-04 20:59:21 +0100

    Remove previous

--- graph.py
+++ graph.py
@@ -4,18 +4,12 @@ from pyrsistent import pmap, pset, thaw
 class Graph:
     def __init__(self):
         self._incidence = pmap() #map<int,set<int>>
-        self._previous = None
         
     def _newgraph(self, incidence):
         a = Graph()
         a._incidence = incidence
-        a._previous = self
         return a
-    
-    @property
-    def previous(self):
-        return self._previous
-        
+            
     def hasvertex(self, v):
         return v in self._incidence
         
--- test_graph.py
+++ test_graph.py
@@ -3,8 +3,6 @@ from graph import Graph
 #I do not use unittesting framework on purpose here to show that you can do quite a lot without it
 
 g1 = Graph()
-assert(g1.previous is None)
-
 assert(g1.hasvertex(1) is False)
 g2 = g1.addvertex(1)
 assert(g2.hasvertex(1) is True)
