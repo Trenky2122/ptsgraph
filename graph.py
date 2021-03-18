10e8ad8932bec23a5faeb74eb8a1dbd241e43af2
Peter Trenčanský <trencanskypeter12@gmail.com>
2021-03-18 11:05:01 +0100

    Added methods for labeling edges and vertices and getting labels of edges and vertices.

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
 from pyrsistent import pmap, pset, thaw
+from labeledint import labeledint
 
 #you may want to use PClass, but we want to learn something
 class Graph:
     def __init__(self):
-        self._incidence = pmap() #map<int,set<int>>
+        self._incidence = pmap() #map<labeledint,set<labeledint>>
         
     def _newgraph(self, incidence):
         a = Graph()
@@ -17,7 +18,7 @@ class Graph:
         if v in self._incidence:
             return self
         else:
-            return self._newgraph(self._incidence.set(v, pset()))
+            return self._newgraph(self._incidence.set(labeledint(v, None), pset()))
             
     def hasedge(self, v1, v2):
         return v2 in self._incidence[v1]
@@ -33,7 +34,7 @@ class Graph:
         return self._newgraph(newinc)
             
     def addedge(self, v1, v2):
-        return self._dostuffwithedge(v1, v2, lambda v: (lambda x: x.add(v)))
+        return self._dostuffwithedge(labeledint(v1, None), labeledint(v2, None), lambda v: (lambda x: x.add(labeledint(v, None))))
                     
     def removeedge(self, v1, v2):
         return self._dostuffwithedge(v1, v2, lambda v: (lambda x: x.remove(v)))
@@ -44,6 +45,30 @@ class Graph:
             evo[v2] = evo[v2].remove(v)
         evo.remove(v)
         return self._newgraph(evo.persistent())
-        
-        
-        
+
+    def labelvertex(self, v, label):
+        if v in self._incidence:
+            newinc = self._incidence.copy()
+            newinc = newinc.remove(v)
+            newinc = newinc.set(labeledint(v, label), pset())
+            return self._newgraph(newinc)
+        else:
+            newg = self.addvertex(v)
+            return newg.labelvertex(v, label)
+
+    def getlabelvertex(self, v):
+        for vt in self._incidence:
+            if vt == v:
+                return vt.label
+
+    def labeledge(self, v1, v2, label):
+        tmpg = self._newgraph(self._incidence)
+        if self.hasedge(v1, v2):
+            tmpg = self.removeedge(v1, v2)
+        return self._dostuffwithedge(v1, v2, lambda v: (lambda x: x.add(labeledint(v, label))))
+
+    def getlabeledge(self, v1, v2):
+        neighbours = self._incidence[v1]
+        for vt in neighbours:
+            if vt == v2:
+                return vt.label
