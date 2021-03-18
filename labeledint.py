1766e39778fc0fc3f8f58a26ba25a5cb3432b35f
Peter Trenčanský <trencanskypeter12@gmail.com>
2021-03-18 19:44:29 +0100

    Added overrides for some integer functions for better transformation ability

--- labeledint.py
+++ labeledint.py
@@ -5,6 +5,26 @@ class labeledint(int):
     def __init__(self, value, label):
         self.__label = label
         int.__init__(self)
+
+    def __add__(self, other):
+        res = super(labeledint, self).__add__(other)
+        return labeledint(res, self.__label)
+
+    def __sub__(self, other):
+        res = super(labeledint, self).__sub__(other)
+        return labeledint(res, self.__label)
+
+    def __mul__(self, other):
+        res = super(labeledint, self).__mul__(other)
+        return labeledint(res, self.__label)
+
+    def __truediv__(self, other):
+        res = super(labeledint, self).__truediv__(other)
+        return labeledint(res, self.__label)
+
+    def __floordiv__(self, other):
+        res = super(labeledint, self).__floordiv__(other)
+        return labeledint(res, self.__label)
     @property
     def label(self):
         return self.__label
